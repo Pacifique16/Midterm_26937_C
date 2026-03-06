58bd1eb6d12225a853f66a246aed60921b366226
Pacifique Harerimana <harerimanapacifique95@gmail.com>
2026-03-06 20:21:10 +0200

    Add main Spring Boot application class

---
+++ src/main/java/com/verifydocs/VerifyDocsApplication.java
@@ -0,0 +1,12 @@
+package com.verifydocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyDocsApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(VerifyDocsApplication.class, args);
+    }
+}
